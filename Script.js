5e4ff1041d0a21fb70021eaaca2b434a2d02ded3
KAVINKUMAR A R <150226309+kavinkumar-ar@users.noreply.github.com>
2024-04-15 17:04:10 +0530

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,69 @@
+let allTasks = [];
+let innerTasks = [];
+let count = 1;
+const upperLi = document.getElementById("upperLi");
+const innerLi = document.getElementById("innerLi");
+const mainForm = document.getElementById("addingForm");
+const mainInput = addingForm.children[0];
+const mainList = document.getElementById("mainList");
+const delAll = document.getElementById("delAll");
+eventListeners();
+loadLS();
+loadInnerTasks();
+function eventListeners() {
+  mainForm.addEventListener("submit", addNewItem);
+  mainList.addEventListener("click", taskStufs);
+  delAll.addEventListener("click", deleteAll);
+}
+function createNewTask(text, check) {
+  const newLi = upperLi.cloneNode(true);
+  newLi.classList.remove("d-none");
+  newLi.setAttribute("id", "");
+  newLi.children[0].setAttribute("href", `#collapse${count}`);
+  newLi.children[0].classList.remove("line-throught");
+  newLi.children[0].innerText = text;
+  if (check === true) {
+    newLi.children[0].classList.add("line-throught");
+  } else {
+    newLi.children[0].classList.remove("line-throught");
+  }
+  newLi.children[3].setAttribute("id", `collapse${count}`);
+
+  mainList.children[0].appendChild(newLi);
+
+  count++;
+  mainInput.value = "";
+}
+function addNewItem(e) {
+  e.preventDefault()
+  const taskText = mainInput.value;
+  if (taskText != "") {
+    allTasks = JSON.parse(localStorage.getItem("allTasks"));
+    let c = false;
+    allTasks.forEach(function (item) {
+      if (item.text == taskText) {
+        c = true;
+        alert("Task Already Added");
+      }
+    });
+    if (c == false) {
+      createNewTask(taskText);
+      saveLS(taskText, 1);
+    }
+  }
+}
+
+function addInnerTask(upperText, mainText, check) {
+  let tempLi = innerLi.cloneNode(true);
+  tempLi.setAttribute("id", "");
+  tempLi.childNodes.forEach(function (item, index) {
+    if (index == 3 || index == 5) {
+      item.classList.remove("d-none");
+    }
+  });
+  tempLi.children[0].innerText = mainText;
+  if (check == true) {
+    tempLi.children[0].classList.add("line-throught");
+  } else {
+    tempLi.children[0].classList.remove("line-throught");
+}
